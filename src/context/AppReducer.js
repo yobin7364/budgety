cfd9272a3e2e2a89bf077f5b3fc4f7d0ad818456
yobin kumar pun <punyobin123@gmail.com>
2020-08-01 11:13:55 +0545

    bug fix

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -8,7 +8,7 @@ export default (state,action) => {
         case 'DELETE_TRANSACTION':
             return {
                 ...state,
-                transactions: state.transactions.filter(transactions => transactions.id !== action)
+                transactions: state.transactions.filter(transactions => transactions.id !== action.payload)
             };
         default:
             return state;
